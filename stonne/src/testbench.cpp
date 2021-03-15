15d5a0a9242a5d9e468f12480a23ab8abbf90be1
jesus.pereza97 <jesus.pereza@um.es>
2021-03-15 18:19:08 +0100

    Commit inicial

--- stonne/src/testbench.cpp
+++ stonne/src/testbench.cpp
@@ -4,6 +4,39 @@
 #include <assert.h>
 #include "types.h"
 
+void max_pooling_layer(unsigned int R, unsigned int S, unsigned int C, unsigned int K, unsigned int G,  unsigned int N, unsigned int X, unsigned int Y, unsigned int strides, 
+float* input, float * outputs) {
+
+    unsigned int OX=(X - R + strides) / strides;
+    unsigned int OY=(Y - S + strides) / strides;
+    K=K/G;
+    C=C/G;
+    unsigned int output_size_n = G*K*OX*OY;
+    unsigned int input_size_n = G*C*X*Y;
+    unsigned int filter_size=R*S*C;
+    unsigned int size_oy=OY*K*G;
+    unsigned int size_y=Y*G*C;
+    for(int n=0; n<N; n++) {
+        for(int g=0; g<G; g++) {
+            for(int k=0; k<K; k++) {
+                for(int ox=0; ox<OX; ox++) {
+                    for(int oy=0; oy<OY; oy++) {
+                        outputs[n*output_size_n + ox*size_oy + oy*K*G + g*K + k]=0.0;
+                        for(int c=0; c<C;c++) {
+                            for(int r=0;r<R;r++) {
+                                for(int s=0;s<S;s++) {
+                                    outputs[n*output_size_n + ox*size_oy + oy*K*G + g*K + k] += input[n*input_size_n+ ox*strides*size_y + oy*strides*C*G + r*size_y + s*C*G + g*C + c];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
+
 
 void sequential_layer(unsigned int R, unsigned int S, unsigned int C, unsigned int K, unsigned int G,  unsigned int N, unsigned int X, unsigned int Y, unsigned int strides, 
 float* input, float* filters, float * outputs) {
