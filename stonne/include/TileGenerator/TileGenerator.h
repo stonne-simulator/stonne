16314db20c0da620fad7d39857260c4c3a18866c
Adrián Fenollar Navarro <adrianfenollarnavarro@gmail.com>
2022-04-16 14:42:22 +0200

    Missing files and compile options

---
+++ stonne/include/TileGenerator/TileGenerator.h
@@ -0,0 +1,12 @@
+#ifndef STONNE_TILEGENERATOR_H
+#define STONNE_TILEGENERATOR_H
+
+namespace TileGenerator {
+
+    class TileGenerator {
+
+    };
+
+} // TileGenerator
+
+#endif //STONNE_TILEGENERATOR_H
