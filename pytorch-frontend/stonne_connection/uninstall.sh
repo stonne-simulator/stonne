04fcf4deee30cc666c9a077510a6545f50b6ae86
Adrián Fenollar Navarro <adrianfenollarnavarro@gmail.com>
2022-04-17 23:32:41 +0200

    Removed files commited by error

--- pytorch-frontend/stonne_connection/uninstall.sh
+++
@@ -1,4 +0,0 @@
-python setup.py install --record files.txt
-xargs rm -rf < files.txt
-rm -rf ~/anaconda3/envs/stonne/lib/python3.7/site-packages/torch_stonne-0.0.0-py3.7-linux-x86_64.egg/
-rm files.txt
