5d37921827fdbba31f8a8b3f76a1ed04da28cf1f
Isai Roman Ballesteros <ac139096@uni-stuttgart.de>
2023-06-13 11:27:29 +0200

    Fixes to avoid Memory Leaks - Missing virtual destructors were preventing the calls of destructors of derived classes. More info in https://www.quantstart.com/articles/C-Virtual-Destructors-How-to-Avoid-Memory-Leaks/)

--- stonne/include/DistributionNetwork.h
+++ stonne/include/DistributionNetwork.h
@@ -21,6 +21,7 @@ private:
 public:
     //General constructor, just used to heritage with unit
     DistributionNetwork(id_t id, std::string name) : Unit(id, name) {}
+    virtual ~DistributionNetwork() {} //Missing destructor call
     //This just executes cycle over all the dsnetworks
     virtual void cycle() {assert(false);} 
     //Get last levels connections together. Useful to connect with mswitches later.
